08e6b3167f77ee431ec0b2e0ba814647aad60d89
Jane <42087697+Iris1e27@users.noreply.github.com>
2022-11-26 00:00:52 -0500

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'http://localhost:5000/posts';
+const url = 'http://52.23.247.164:5000/posts';
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
